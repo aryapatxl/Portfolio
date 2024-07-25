fd7c99be5070b87ed357e6659d7e50c5197ad3b8
aryapatxl <aryaxrp@gmail.com>
2024-07-25 19:57:49 -0400

    added github button

--- arya-portfolio/src/App.js
+++ arya-portfolio/src/App.js
@@ -1,18 +1,25 @@
-import RT from './ResponsiveText';
 import "./global.css"
 import './App.css';
-
+import githubLogo from './images/githubLogo.png'
+import LinkedinLogo from '/images/LinkedinLogo.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header" role="banner">
-        <h1>Arya Patel</h1>
-        
-
+        <h1 className="rakkas-regular">Arya Patel</h1>
+        <div className="button-container">
+          <a href="https://github.com/aryapatxl" target="_blank" rel="github">
+            <img src={githubLogo} alt="GitHub Logo" className="image-button" />
+          </a>
+          <a href="https://www.linkedin.com/in/aryapatel-/" target="_blank" rel="github">
+            <img src={LinkedinLogo} alt="GitHub Logo" className="image-button" />
+          </a>
+        </div>
       </header>
-      <body>
-        <p>asdflkasdj;fakdfkas</p>
-      </body>
+      <main>
+        
+        <p className="rakkas-regular">asdflkasdj;fakdfkas</p>
+      </main>
     </div>
   );
 }
