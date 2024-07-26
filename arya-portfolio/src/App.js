aa81e0b03c299efa82a6f6622a42d46e9a48e89f
aryapatxl <aryaxrp@gmail.com>
2024-07-26 12:12:44 -0400

    social media links

--- arya-portfolio/src/App.js
+++ arya-portfolio/src/App.js
@@ -1,7 +1,9 @@
 import "./global.css"
 import './App.css';
 import githubLogo from './images/githubLogo.png'
-import LinkedinLogo from '/images/LinkedinLogo.png'
+import LinkedinLogo from './images/LinkedinLogo.png'
+import CameraIcon from './images/CameraIcon.png'
+import EmailIcon from './images/emailIcon.png'
 function App() {
   return (
     <div className="App">
@@ -11,8 +13,14 @@ function App() {
           <a href="https://github.com/aryapatxl" target="_blank" rel="github">
             <img src={githubLogo} alt="GitHub Logo" className="image-button" />
           </a>
+          <a href="mailto:aryaxrp@gmail.com" target="_blank" rel="github">
+            <img src={EmailIcon} alt="Email Logo" className=" image-button email-button" />
+          </a>
           <a href="https://www.linkedin.com/in/aryapatel-/" target="_blank" rel="github">
             <img src={LinkedinLogo} alt="GitHub Logo" className="image-button" />
+          </a>
+          <a href="https://aryapatelx.wordpress.com/" target="_blank" rel="github">
+            <img src={CameraIcon} alt="Camera Logo" className=" image-button camera-button" />
           </a>
         </div>
       </header>
